9d6a82c03fb39284b2c50b68e27d0a676d7500c3
AVSDheeraj120904 <163455970+AVSDheeraj120904@users.noreply.github.com>
2024-08-17 16:54:42 +0530

    Update page.tsx
    updated

--- fmc-24-nextjs/app/page.tsx
+++ fmc-24-nextjs/app/page.tsx
@@ -5,12 +5,10 @@ import group from "../public/Group 176.png"
 export default function Home() {
   return (
   <div className="flex flex-wrap justify-center items-center ">
-    <div className="absolute top-0 z-10 flex items-center justify-center w-full mt-[5961px]">    
-     <div className="absolute top-0 z-10 flex items-center justify-center w-full mt-[5961px]">   
+    <div className="absolute top-0 z-10 flex items-center justify-center w-full mt-[5961px]">  
        <div className="font-clash text-[104px] font-bold leading-[127.92px] text-center">
          Previous Sponsors
-       </div> 
-     </div>
+       </div>
     </div>
 
     <div className="absolute top-0 z-0 flex items-center justify-center w-full mt-[6025px]">
